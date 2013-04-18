c6c30e7dd80acaa7deb0eed899705171ebe983bb
dvdrtrgn <dvdrtrgn@gmail.com>
2013-04-18 17:30:37 -0500

    tidy

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function main() {
     $('.load').each(function () {
         var me = $(this),
             bt = me.prev();
-        clog(me, bt);
         bt.text('Load ' + bt.text());
         bt.on('click', function () {
             $('#vals').text(me.text());
