0b641eab674aefdd06527d3b14cdb95d2787d0b2
dvdrtrgn <dvdrtrgn@gmail.com>
2013-04-18 23:28:14 -0500

    loop closure

--- jq-xsv.js
+++ jq-xsv.js
@@ -8,11 +8,10 @@
         objArr = [];
 
     function parseXSVLine(sep, line) {
-        var i, j, chunk, quote;
-
         line = line.split(sep);
         // check for splits performed inside quoted strings and correct if needed
-        for (i = 0; i < line.length; i++) {
+        $.each(line, function (i, e) {
+            var j, chunk, quote;
             chunk = $.trim(line[i]);
             quote = '';
             if (chunk.charAt(0) === '"' || chunk.charAt(0) === "'") {
@@ -36,8 +35,8 @@
                     line.splice(j, 1);
                 }
             }
-        }
-        for (i = 0; i < line.length; i++) {
+        });
+        $.each(line, function (i, e) {
             line[i] = $.trim(line[i]);
             // remove leading/trailing quotes
             if (line[i].charAt(0) === '"') {
@@ -45,46 +44,51 @@
             } else if (line[i].charAt(0) === "'") {
                 line[i] = line[i].replace(/^'|'$/g, '');
             }
-        }
+        });
         return line;
     }
 
     function valsToJson(sep) {
         var f = document.forms.convertForm,
             valsText = f.elements.vals.value,
-            jsonText = '',
-            i, j;
-
-        if (valsText === '') {
-            throw new Error('Missing source data.');
-        } else {
-            valsText = reQuote(valsText); // preserve gaps
+            jsonText = '';
+        $('html').removeClass('error');
+        try {
+            if (valsText === '') {
+                throw new Error('Missing source data.');
+            }
+            if (sep === ',') {
+                valsText = reQuote(valsText); //    preserve gaps
+            }
             valsRows = valsText.split(/[\r\n]/g); // split into rows
             // get rid of empty rows
-            for (i = 0; i < valsRows.length; i++) {
+            $.each(valsRows, function (i, e) {
                 if ($.trim(valsRows[i]) === '') {
                     valsRows.splice(i, 1);
                     i--;
                 }
-            }
+            });
             if (valsRows.length < 2) {
                 throw new Error('Data missing header row?');
-            } else {
-                objArr = [];
-                for (i = 0; i < valsRows.length; i++) {
-                    valsRows[i] = parseXSVLine(sep, valsRows[i]);
-                }
-                for (i = 1; i < valsRows.length; i++) {
-                    if (valsRows[i].length > 0) {
-                        objArr.push({});
-                    }
-                    for (j = 0; j < valsRows[i].length; j++) {
-                        objArr[i - 1][valsRows[0][j]] = valsRows[i][j];
-                    }
-                }
-                jsonText = JSON.stringify(objArr, null, '\t');
-                f.elements.json.value = jsonText;
             }
+            objArr = [];
+            $.each(valsRows, function (i, e) {
+                valsRows[i] = parseXSVLine(sep, valsRows[i]);
+            });
+            $.each(valsRows, function (i, e) {
+                if (!i) return;
+                if (valsRows[i].length > 0) {
+                    objArr.push({});
+                }
+                $.each(valsRows[i], function (j, e) {
+                    objArr[i - 1][valsRows[0][j]] = valsRows[i][j];
+                });
+            });
+            jsonText = JSON.stringify(objArr, null, '\t');
+            f.elements.json.value = jsonText;
+        } catch (err) {
+            $('html').addClass('error');
+            console.error(err.toString());
         }
     }
 
@@ -93,7 +97,7 @@
         var quo = str.match(/"[^"]+"/g),
             non = str.split(/"[^"]+"/g),
             neo = [];
-        quo.length && $.each(quo, function(i, e){
+        quo && $.each(quo, function(i, e) {
             // escape line breaks within
             e = e.replace(/\n/g, '\\n');
             neo.push(non[i], e);
