f834fbe9377ed01e26c6a027f7b49166365ce754
dvdrtrgn <dvdrtrgn@gmail.com>
2013-04-22 15:41:20 -0500

    keyhoist

--- data/script.js
+++ data/script.js
@@ -17,17 +17,23 @@ function main() {
             p.appendTo(ele);
         });
     }
+    function keyhoist(arr, num) {
+        var key = arr[num].key;
+        clog(key, num)
+        return key || (num + 1);
+    }
     $.each(DATA, function(i, DIV) {
         var div = $('<div>').appendTo('body');
         $('<h1>').text(i).appendTo(div);
 
         $.each(DIV, function(i, SEC) {
             var sec = $('<section>').appendTo(div);
-            $('<h2>').text(i).appendTo(sec);
+            $('<h2>').text(i).appendTo(sec).before($('<a id='+i+'>'));
 
             $.each(SEC, function(i, ART) {
                 var art = $('<article>').appendTo(sec);
-                $('<h3>').text('article ' + (i+1)).appendTo(art);
+                i = keyhoist(SEC, i);
+                $('<h3>').text('article ' + i).appendTo(art);
                 textify(ART, art);
             });
         });
