027089e432ef09f115de17b9980a94a114785dfb
dvdrtrgn <dvdrtrgn@gmail.com>
2013-04-22 17:15:06 -0500

    fancy up a bit

--- data/script.js
+++ data/script.js
@@ -1,5 +1,5 @@
 /*jslint es5:true, white:false */
-/*globals $, clog */
+/*globals $, Components, Compositions, Giving, Support, clog, console */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 function main() {
@@ -13,52 +13,56 @@ function main() {
     var nav = $('nav');
 
     function textify(obj, ele) {
-        $.each(obj, function(i, e) {
-            var t = ('<strong>' +i + '</strong>: ') + e.replace(/\\n/g, '<br>'),
-            p =  $('<p>').html(t);
+        $.each(obj, function (i, e) {
+            var t = ('<strong>' + i + '</strong>: ') + e.replace(/\\n/g, '<br>'),
+                p = $('<p>').html(t);
             p.appendTo(ele);
         });
     }
+
     function keyhoist(arr, num) {
         var key = arr[num].key;
         if (key) {
-            clog(key, num)
+            clog(key, num);
             arr[key] = arr[num];
         }
         return key || (num + 1);
     }
-    $.each(DATA, function(i1, DIV) {
-        var div = $('<div>').appendTo('body');
-        anchor(i1, $('<h1>').text(i1).appendTo(div), '<h4>' );
-
-        $.each(DIV, function(i2, SEC) {
-            var sec = $('<section>').appendTo(div);
-            anchor(i2, $('<h2>').text(i2).appendTo(sec), '<p>' );
-
-            $.each(SEC, function(i3, ART) {
-                var art = $('<article>').appendTo(sec);
-                i3 = keyhoist(SEC, i3);
-                anchor([i2,i3], $('<h3>').text('article ' + i3).appendTo(art) );
-
-                textify(ART, art);
-            });
-        });
-    });
 
     function anchor(str, ele, wrap) {
         var anc, lnk, pre = '';
+
         if (typeof str === 'object') {
             pre = str[0] + '-';
             str = str[1];
         }
         wrap = wrap || '<button>';
+
         // add anchor
         anc = $('<a id="' + pre + str + '">');
         ele.before(anc);
 
         // make nav link
-        lnk = $('<a href="#'+ pre + str +'">');
+        lnk = $('<a href="#' + pre + str + '">');
         lnk.text(str);
         lnk.appendTo(nav).wrap(wrap);
     }
+
+    $.each(DATA, function (i1, DIV) {
+        var div = $('<div>').appendTo('body');
+        anchor(i1, $('<h1>').text(i1).appendTo(div), '<h4>');
+
+        $.each(DIV, function (i2, SEC) {
+            var sec = $('<section>').appendTo(div);
+            anchor(i2, $('<h2>').text(i2).insertBefore(sec), '<p>');
+
+            $.each(SEC, function (i3, ART) {
+                var art = $('<article>').appendTo(sec);
+                i3 = keyhoist(SEC, i3);
+                anchor([i2, i3], $('<h3>').text('article ' + i3).insertBefore(art));
+
+                textify(ART, art);
+            });
+        });
+    });
 }
