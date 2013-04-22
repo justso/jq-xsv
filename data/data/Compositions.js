bffaa0aa3384d78988e249195cc9b612ae60b9e2
dvdrtrgn <dvdrtrgn@gmail.com>
2013-04-22 16:52:03 -0500

    tweaks

--- data/data/Compositions.js
+++ data/data/Compositions.js
@@ -86,10 +86,10 @@ var Compositions = {
         "content": "Thank you for visiting the Wells Fargo Sibos webpage. Our Global Financial Institutions team has an exciting program lined up for you during the week of the conference, and we look forward to seeing you in Osaka.\\n\\nFor more information about Sibos 2012, please visit the official Sibos website at sibos.com."
     }, {
         "heading": "Sibos events",
-        "content": "EVENTS/ celebrate\\nEVENTS/ trans\\nEVENTS/ booth"
+        "content": "REF:EVENTS/ celebrate\\nREF:EVENTS/ trans\\nREF:EVENTS/ booth"
     }, {
         "heading": "Visit our booth",
-        "content": "FILES/ 4B06"
+        "content": "REF:FILES/ 4B06"
     }, {
         "heading": "Get involved",
         "content": "Wells Fargo is proud to partner with GlobalGiving.org again to offer you a wonderful program that gives back to our communities"
@@ -97,7 +97,6 @@ var Compositions = {
         "heading": "Highlights from Sibos",
         "content": "Follow news from the Wells Fargo booth and daily Sibos conference highlights, including photos and videos.\\n\\n*The content in this section is provided for general informational purposes only and represents the individuals’ opinions on what they deemed relevant Sibos content. Wells Fargo does not endorse and is not responsible for the content, links, privacy policy, or security policy of the website."
     }],
-
     PHOTOS: [{
         "heading": "Photo Gallery",
         "content": "Photographs from Sibos 2012 Sunday reception.\\n\\n['IMG_6201', 'IMG_6120', 'IMG_6122', 'IMG_6133', 'IMG_6134', 'IMG_6136', 'IMG_6138', 'IMG_6139', 'IMG_6140', 'IMG_6142', 'IMG_6143', 'IMG_6146', 'IMG_6148', 'IMG_6154', 'IMG_6156', 'IMG_6159', 'IMG_6168', 'IMG_6172', 'IMG_6174', 'IMG_6175', 'IMG_6177', 'IMG_6179', 'IMG_6180', 'IMG_6183', 'IMG_6184', 'IMG_6187', 'IMG_6188', 'IMG_6189', 'IMG_6191', 'IMG_6196', 'IMG_6200', 'IMG_6202', 'IMG_6203', 'IMG_6204', 'IMG_6205', 'IMG_6206', 'IMG_6207', 'IMG_6209', 'IMG_6210', 'IMG_6211', 'IMG_6213', 'IMG_6214', 'IMG_6215', 'IMG_6216', 'IMG_6218', 'IMG_6220', 'IMG_6221', 'IMG_6222', 'IMG_6223', 'IMG_6224', 'IMG_6225', 'IMG_6226', 'IMG_6229', 'IMG_6230', 'IMG_6231', 'IMG_6233', 'IMG_6234', 'IMG_6235', 'IMG_6236', 'IMG_6238', 'IMG_6239', 'IMG_6241', 'DSC_0200', 'DSC_0210', 'DSC_0211', 'DSC_0219', 'DSC_0220', 'DSC_0222', 'DSC_0223', 'DSC_0224', 'DSC_0226', 'DSC_0229', 'DSC_0230', 'DSC_0231', 'DSC_0234', 'DSC_0235', 'DSC_0236', 'DSC_0237', 'DSC_0238', 'DSC_0239', 'DSC_0240', 'DSC_0241', 'DSC_0242', 'DSC_0244', 'DSC_0245', 'DSC_0246', 'DSC_0247', 'DSC_0248', 'DSC_0249', 'DSC_0250', 'DSC_0251', 'DSC_0252', 'DSC_0253', 'DSC_0254', 'DSC_0255', 'DSC_0256', 'DSC_0258', 'DSC_0259', 'DSC_0260', 'DSC_0261', 'DSC_0262', 'DSC_0263', 'DSC_0264', 'DSC_0265', 'DSC_0266', 'DSC_0267', 'DSC_0268', 'DSC_0269', 'DSC_0270', 'DSC_0271', 'DSC_0273', 'DSC_0274', 'DSC_0275', 'DSC_0277', 'DSC_0280', 'DSC_0281', 'DSC_0292',]"
@@ -108,12 +107,12 @@ var Compositions = {
     }],
     VISIT: [{
         "heading": "Visit our booth",
-        "content": "As a repeat Sibos exhibitor, we’re proud to give our customers an opportunity to visit and interact with our business leaders and relationship managers at our booth, No. 4B06.\\nFILES/ map  |  FILES/ booth\\n\\nStop by to take your picture in front of the Buffalo Bill stagecoach and learn more about our history. And “check in” with your smart phone or PDA device to receive a free gift."
+        "content": "As a repeat Sibos exhibitor, we’re proud to give our customers an opportunity to visit and interact with our business leaders and relationship managers at our booth, No. 4B06.\\nREF:FILES/ map  |  REF:FILES/ booth\\n\\nStop by to take your picture in front of the Buffalo Bill stagecoach and learn more about our history. And “check in” with your smart phone or PDA device to receive a free gift."
     }, {
         "heading": "Events",
-        "content": "We’re hosting a variety of events during the Sibos conference, including our annual Sibos Celebration reception at the elegant Taiko-en, and a booth welcome reception.\\n\\nEVENTS/ celebrate\\nEVENTS/ trans\\nEVENTS/ booth"
+        "content": "We’re hosting a variety of events during the Sibos conference, including our annual Sibos Celebration reception at the elegant Taiko-en, and a booth welcome reception.\\n\\nREF:EVENTS/ celebrate\\nREF:EVENTS/ trans\\nREF:EVENTS/ booth"
     }, {
-        "heading": "Speakers\\n",
-        "content": "PEEPS/ George\\nEVENTS/ pmpg\\nEVENTS/ payments\\nEVENTS/ global\\n\\nPEEPS/ Steve\\nEVENTS/ brave"
+        "heading": "Speakers",
+        "content": "PEEPS/ George\\nREF:EVENTS/ pmpg\\nREF:EVENTS/ payments\\nREF:EVENTS/ global\\n\\nPEEPS/ Steve\\nREF:EVENTS/ brave"
     }],
 };
