c6ff34a07e518dfeee10296b2df5dddbb47d8613
dvdrtrgn <dvdrtrgn@gmail.com>
2013-04-22 17:57:38 -0500

    reorg

--- data/data/Support.js
+++ data/data/Support.js
@@ -1,89 +1,12 @@
 var Support = {
-    _BLURBS: [{
+    BLURBS: [{
         "key": "slug",
         "content": "© 2012 Wells Fargo Bank, N.A. All rights reserved. ECG-727744"
     }, {
         "key": "share",
-        "content": "<div id=\"\"sharebar\"\">\\n            <script src=\"\"http://w.sharethis.com/button/buttons.js\"\"></script>\\n            <p class=\"\"cap\"\">Share this page                <!-- -->\\n                <span id=\"\"st_twitter\"\"></span>                <!-- -->\\n                <span id=\"\"st_linkedin\"\"></span>                <!-- -->\\n                <span id=\"\"st_email\"\"></span>                <!-- -->\\n                <span id=\"\"st_facebook\"\"></span>                <!-- -->\\n                <span id=\"\"st_googleplus\"\"></span>                <!-- -->\\n                <span id=\"\"st_pinterest\"\"></span>                <!-- -->\\n            </p>\\n            <script src=\"\"../script/share.js\"\"></script>\\n        </div>"
+        "content": "&lt;div id=&quot;sharebar&quot;&gt;\\n &lt;script src=&quot;http://w.sharethis.com/button/buttons.js&quot;&gt;&lt;/script&gt;\\n &lt;p class=&quot;cap&quot;&gt;Share this page &lt;!-- --&gt;\\n &lt;span id=&quot;st_twitter&quot;&gt;&lt;/span&gt; &lt;!-- --&gt;\\n &lt;span id=&quot;st_linkedin&quot;&gt;&lt;/span&gt; &lt;!-- --&gt;\\n &lt;span id=&quot;st_email&quot;&gt;&lt;/span&gt; &lt;!-- --&gt;\\n &lt;span id=&quot;st_facebook&quot;&gt;&lt;/span&gt; &lt;!-- --&gt;\\n &lt;span id=&quot;st_googleplus&quot;&gt;&lt;/span&gt; &lt;!-- --&gt;\\n &lt;span id=&quot;st_pinterest&quot;&gt;&lt;/span&gt; &lt;!-- --&gt;\\n &lt;/p&gt;\\n &lt;s cript src=&quot;../script/share.js&quot;&gt;&lt;/script&gt;\\n &lt;/div&gt;"
     }],
-    _COMPS: [{
-        "key": "footer",
-        "content": "_BLURBS/ slug + _BLURBS/ share"
-    }],
-    _EVENTS: [{
-        "key": "celebrate",
-        "order": "1",
-        "day": "Sunday, October 28",
-        "title": "Annual Sibos Celebration",
-        "Speaker": "NA",
-        "start": "5:00 p.m.",
-        "end": "8:00 p.m.",
-        "place": "Taiko-en",
-        "link": "LINKS/ 727872"
-    }, {
-        "key": "brave",
-        "order": "2",
-        "day": "Monday, October 29",
-        "title": "Brave new leaders in IT",
-        "Speaker": "Steve Ellis",
-        "start": "4:00 p.m.",
-        "end": "5:00 p.m.",
-        "place": "",
-        "link": "LINKS/ id-124"
-    }, {
-        "key": "trans",
-        "order": "3",
-        "day": "Tuesday, October 30",
-        "title": "Transparency: The new paradigm impacting Global Transaction Banking",
-        "Speaker": "NA",
-        "start": "3:00 p.m.",
-        "end": "4:00 p.m.",
-        "place": "Katsura Room L5; Hyatt Regency Hotel"
-    }, {
-        "key": "Please speak with your relationship manager about availability.\"",
-        "order": "LINKS/ 734818"
-    }, {
-        "key": "booth",
-        "order": "4",
-        "day": "Tuesday, October 30",
-        "title": "Booth reception invite",
-        "Speaker": "NA",
-        "start": "4:00 p.m.",
-        "end": "5:30 p.m.",
-        "place": "Wells Fargo booth No. 4B06, Hall 4",
-        "link": "LINKS/ 727874"
-    }, {
-        "key": "pmpg",
-        "order": "5",
-        "day": "Wednesday, October 31",
-        "title": "PMPG – What’s up in the payments market place",
-        "Speaker": "George Doolittle",
-        "start": "9:30 a.m.",
-        "end": "10:30 a.m.",
-        "place": "Community room 3",
-        "link": "LINKS/ id-286"
-    }, {
-        "key": "payments",
-        "order": "6",
-        "day": "Wednesday, October 31",
-        "title": "Consumer payments and increasing regulatory requirements. The need for more transparency",
-        "Speaker": "George Doolittle",
-        "start": "4:00 p.m.",
-        "end": "5:00 p.m.",
-        "place": "Community room 2",
-        "link": "LINKS/ id-279"
-    }, {
-        "key": "global",
-        "order": "7",
-        "day": "Thursday, November 1",
-        "title": "Global transaction banking: In need of business insights?",
-        "Speaker": "George Doolittle",
-        "start": "11:00 a.m.",
-        "end": "12:00 p.m.",
-        "place": "Conference room 2",
-        "link": "LINKS/ id-104"
-    }],
-    _FILES: [{
+    FILES: [{
         "key": "map",
         "group": "images",
         "subgroup": "misc",
@@ -156,7 +79,7 @@ var Support = {
         "text": "[View bio] (PDF*)",
         "path": "media/Steve_Ellis-bio.pdf"
     }],
-    _LINKS: [{
+    LINKS: [{
         "key": "727872",
         "text": "Space is limited, [RSVP here]",
         "src": "http://www.wellsfargomedia.com/sibos2012/email/727872/index.html"
@@ -208,20 +131,5 @@ var Support = {
         "key": "today",
         "text": "WF Today piece",
         "src": "https://www.wellsfargo.com/downloads/pdf/about/wellsfargotoday.pdf"
-    }],
-    _PEEPS: [{
-        "key": "george",
-        "Speakers": "George Doolittle",
-        "title": "Head of Wells Fargo Global Payment Services",
-        "src.pic": "FILES/ george",
-        "src.vid": "FILES/ george-vid",
-        "src.bio": "FILES/ george-bio"
-    }, {
-        "key": "steve",
-        "Speakers": "Steve Ellis",
-        "title": "Head of Wells Fargo Wholesale Services", /*bad token " "*/
-        "src.pic": "FILES/ steve",
-        "src.vid": "FILES/steve-vid",
-        "src.bio": "FILES/steve-bio"
     }],
 };
--- data/script.js
+++ data/script.js
@@ -1,37 +1,72 @@
 /*jslint es5:true, white:false */
-/*globals $, clog */
+/*globals $, Components, Compositions, Giving, Support, clog, console */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 function main() {
     console.log('main');
-    //    for each in data
-    // create div
-    // create section
-    // create article
-    var body = $('body');
+    DATA = {
+        Support: Support,
+        Components: Components,
+        Giving: Giving,
+        Compositions: Compositions,
+    };
+    var nav = $('nav');
 
     function textify(obj, ele) {
-        $.each(obj, function(i, e) {
-            var p =  $('<p>')
-            .append('<strong>' +i + '</strong>: ')
-            .append(e);
-            p.appendTo(ele)
+        $.each(obj, function (i, e) {
+            var t = ('<strong>' + i + '</strong>: ') + e.replace(/\\n/g, '<br>'),
+                p = $('<p>').html(t);
+            p.appendTo(ele);
         });
     }
-    $.each(DATA, function(i, DIV) {
-        var div = $('<div>').appendTo(body);
-        $('<h1>').text(i).appendTo(div);
 
-        $.each(DIV, function(i, SEC) {
+    function keyhoist(arr, num) {
+        var key = arr[num].key;
+        if (key) {
+            clog(key, num);
+            arr[key] = arr[num];
+        }
+        return key || (num + 1);
+    }
+
+    function pretreat() {
+        //    search for x-refs %_
+    }
+
+    function anchor(str, ele, wrap) {
+        var anc, lnk, pre = '';
+
+        if (typeof str === 'object') {
+            pre = str[0] + '-';
+            str = str[1];
+        }
+        wrap = wrap || '<button>';
+
+        // add anchor
+        anc = $('<a id="' + pre + str + '">');
+        ele.before(anc);
+
+        // make nav link
+        lnk = $('<a href="#' + pre + str + '">');
+        lnk.text(str);
+        lnk.appendTo(nav).wrap(wrap);
+    }
+
+    $.each(DATA, function (i1, DIV) {
+        var div = $('<div>').appendTo('body');
+        anchor(['doc', i1], $('<h1>').text(i1).appendTo(div), '<h4>');
+
+        $.each(DIV, function (i2, SEC) {
             var sec = $('<section>').appendTo(div);
-            $('<h2>').text(i).appendTo(sec);
+            anchor([i1, i2], $('<h2>').text(i2).insertBefore(sec), '<p>');
 
-            $.each(SEC, function(i, ART) {
+            $.each(SEC, function (i3, ART) {
                 var art = $('<article>').appendTo(sec);
-                $('<h3>').text('article ' + i).appendTo(art);
+                i3 = keyhoist(SEC, i3);
+                anchor([i2, i3], $('<h3>').text(i2 + '/' + i3).insertBefore(art));
+
                 textify(ART, art);
             });
         });
     });
-
 }
